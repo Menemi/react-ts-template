64fa7350a86315f6d470a05a94600ed0fae136af
Титов Даниил Ярославович <da.y.titov@tinkoff.ru>
2025-07-19 22:17:43 +0300

    fix: npm start errors & warnings

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,7 @@ import styles from './App.module.css';
 import React from 'react';
 
 const App = () => {
-    return <div className={styles.container}>
-        hello world!
-    </div>;
+    return <div className={styles.container}>hello world!</div>;
 };
 
 export default App;
